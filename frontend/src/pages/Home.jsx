0c179b5baee5535ebbaf2bb8b00792d58a533b8a
pareshanmishra98 <prashant908mishra@gmail.com>
2026-06-24 20:08:35 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Banner from "../components/Banner"
 import Header from "../components/Header"
-import SpecialityMenu from "../components/specialitymenu"
+import SpecialityMenu from "../components/specialityMenu"
 import TopDoctors from "../components/TopDoctors"
 
 const Home = () => {
